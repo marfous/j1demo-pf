55c34a2014d15378172dc6c2662fc07ac8870455
Martin Fousek <martin.fousek@gmail.com>
2013-08-19 16:34:18 +0200

    Samples for the new EL3.0 operators

---
+++ 03-ExpressionLanguage3/src/main/java/javaone/con3638/expressionlanguage3/OperatorsBean.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 Martin Fousek & Mert Caliskan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package javaone.con3638.expressionlanguage3;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.inject.Named;
+
+/**
+ *
+ * @author marfous
+ */
+@Named
+@javax.enterprise.context.RequestScoped
+public class OperatorsBean {
+
+    public List<String> getCarBrands() {
+        return Arrays.asList("Škoda", "BMW", "Ford", "Toyota", "Mazda", "Mercedes");
+    }
+
+}
